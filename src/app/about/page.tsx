809f7a2a00603bbdb35ce3699a6b968c165a7ee5
Rachata Boontiang <mju6804101373@mju.ac.th>
2026-08-17 18:46:57 +0700

    setup

---
+++ src/app/about/page.tsx
@@ -0,0 +1,16 @@
+export default function AboutPage() {
+    return (
+        <main className="page">
+        <h1>About Me</h1>
+        
+        <section className="aboutCard">
+            <h2>ประวัติส่วนตัว</h2>
+            <p>นาย รชต สุดหล่อ กำลังศึกษาอยู่ที่มหาวิทยาลัยแม่โจ้</p>
+            <p>I Love Programming</p>
+            <h2>I like Phat Kaprao Mu Krop</h2>
+            <h3>weight: 60 kg Height: 166 cm</h3>
+            <p>LOL รักเธอคนนี้ 24 ชม.</p>
+        </section>
+        </main>
+    );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
+      {/*{children} เอาลูกๆของมันเข้ามาต่อ */}
     </html>
   );
 }
